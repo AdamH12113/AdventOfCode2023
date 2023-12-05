b5579aa0bd15373c88e604f1e578c19043c61cdf
Adam Haun <adam.haun@microsiliconinc.com>
2023-12-05 12:37:35 -0600

    Finished part 1 of day 5.

--- Day5.py
+++ Day5.py
@@ -15,4 +15,36 @@ except Exception as e:
 input_blocks = input_text.split('\n\n')
 seeds = [int(n) for n in re.findall(r'\d+', input_blocks[0])]
 
-print(input_lines)
+maps = []
+for block in range(1, len(input_blocks)):
+	maps.append([])
+	lines = input_blocks[block].split('\n')[1:]
+	for line in lines:
+		nums = [int(n) for n in re.findall(r'\d+', line)]
+		maps[block - 1].append({'dest': nums[0], 'src': nums[1], 'range': nums[2]})
+
+def do_mapping(value, maps):
+	for map in maps:
+		if value >= map['src'] and value <= map['src'] + map['range']:
+			return map['dest'] + (value - map['src'])
+	return value
+
+mapped_values = seeds
+for vmap in maps:
+	mfunc = lambda value: do_mapping(value, vmap)
+	mapped_values = list(map(mfunc, mapped_values))
+print(f"Part 1: The lowest location value is: {min(mapped_values)}")
+
+# Part 2: The seed numbers now describe ranges. Each pair of numbers represents a starting value
+# and a range. The real input represents billions of seeds. This cannot be brute-forced.
+
+
+
+
+
+
+
+
+
+
+
