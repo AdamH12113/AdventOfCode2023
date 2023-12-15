2fd193be418cba42fab67493d14485a100a42275
Adam Haun <adamhaun@gmail.com>
2023-12-15 01:03:47 -0600

    Completed day 5.

--- Day5.py
+++ Day5.py
@@ -26,7 +26,7 @@ for block in range(1, len(input_blocks)):
 
 def do_mapping(value, maps):
 	for map in maps:
-		if value >= map['src'] and value <= map['src'] + map['range']:
+		if value >= map['src'] and value < map['src'] + map['range']:
 			return map['dest'] + (value - map['src'])
 	return value
 
@@ -37,9 +37,44 @@ for vmap in maps:
 print(f"Part 1: The lowest location value is: {min(mapped_values)}")
 
 # Part 2: The seed numbers now describe ranges. Each pair of numbers represents a starting value
-# and a range. The real input represents billions of seeds. This cannot be brute-forced.
+# and a range. The real input represents billions of seeds. This cannot be brute-forced. But (and
+# I must admit I found this hint online) we can reverse the maps and start searching from the low
+# end of the location range looking for a seed that's in one of our seed ranges. This is still
+# brute force, but starting out with a sparse search lets me tune in on the right value by hand.
+def do_reverse_mapping(value, rmaps):
+	for map in rmaps:
+		if value >= map['dest'] and value < map['dest'] + map['range']:
+			return map['src'] + (value - map['dest'])
+	return value
 
+def get_seed_from_location(location, rev_maps):
+	val = location
+	for rmap in rev_maps:
+		val = do_reverse_mapping(val, rmap)
+	return val
 
+def get_location_from_seed(seed, all_maps):
+	val = seed
+	for vmap in all_maps:
+		val = do_mapping(val, vmap)
+	return val
+
+seed_ranges = []
+for s in range(0, len(seeds), 2):
+	seed_ranges.append((seeds[s], seeds[s+1]))
+
+def seed_in_ranges(seed, ranges):
+	for r in ranges:
+		if seed >= r[0] and seed < r[0] + r[1]:
+			return True
+	return False
+
+reversed_maps = list(reversed(maps))
+for location in range(20000000, 21000000, 1):
+	seed = get_seed_from_location(location, reversed_maps)
+	if seed_in_ranges(seed, seed_ranges):
+		print(f"Part 2: The lowest location that corresponds to a given seed is: {location}")
+		break
 
 
 
