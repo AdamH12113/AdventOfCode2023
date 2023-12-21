1251efb18f5e9395a12585a850ea29ae890b9e8f
Adam Haun <adamhaun@gmail.com>
2023-12-20 23:12:38 -0600

    Completed day 17, kind of. Completed part 1 of day 20.

--- Day17.py
+++ Day17.py
@@ -27,12 +27,12 @@ def print_grid(grid: list):
 # steps in any direction. Time for a breadth-first search! (I'm surprised its taken this long to
 # need one, honestly.) Since the search state is a bit more complicated than just coordinates, I'm
 # going to take this opportunity to experiment with named tuples.
-
-# Internet suggests making a tuple of coords, direction, and steps and doing the BFS on that
-
-
+#
+# The internet suggested making a tuple of coords, direction, and steps and doing the BFS on that.
+# This takes forever to run but does get the correct answer.
 Coord = namedtuple('Coord', ['x', 'y'])
-State = namedtuple('State', ['coord', 'dir', 'dirsteps', 'loss'])
+Node = namedtuple('Node', ['coord', 'dir', 'dirsteps'])
+State = namedtuple('State', ['node', 'loss'])
 up = Coord(0, -1)
 down = Coord(0, 1)
 left = Coord(-1, 0)
@@ -43,7 +43,8 @@ def add_coords(a: Coord, b: Coord):
 
 # Start with zero steps in the current direction as a hack to handle the first tile
 visited = {}
-next_steps = [State(Coord(0, 0), right, 0, 0)]
+start_node = Node(Coord(0, 0), right, 0)
+next_steps = [State(start_node, 0)]
 
 def print_costs():
 	for y in range(y_size):
@@ -57,14 +58,16 @@ def print_costs():
 
 while len(next_steps) > 0:
 	# Update the minimum loss for the current location
-	loc, direction, dirsteps, loss = next_steps.pop()
-	if loc not in visited or visited[loc] > loss:
-		visited[loc] = loss
-		#print(len(next_steps), len(visited))
+	node, loss = next_steps.pop()
+	if node not in visited or visited[node] > loss:
+		if len(next_steps) % 1000 == 0:
+			print(len(next_steps), node, loss)
+		visited[node] = loss
 	else:
 		continue
 
 	# Which ways can we physically move?
+	loc, direction, dirsteps = node
 	can_move_left = loc.x > 0 and not (direction == right or (direction == left and dirsteps >= 3))
 	can_move_right = loc.x < x_size - 1 and not (direction == left or (direction == right and dirsteps >= 3))
 	can_move_up = loc.y > 0 and not (direction == down or (direction == up and dirsteps >= 3))
@@ -75,36 +78,114 @@ while len(next_steps) > 0:
 		new_coord = add_coords(loc, left)
 		new_loss = loss + grid[new_coord.y][new_coord.x]
 		new_dirsteps = (dirsteps + 1) if direction == left else 1
-		if new_coord not in visited or visited[new_coord] > new_loss:
-			new_state = State(new_coord, left, new_dirsteps, new_loss)
+		new_node = Node(new_coord, left, new_dirsteps)
+		if new_node not in visited or visited[new_node] >= new_loss:
+			new_state = State(new_node, new_loss)
 			next_steps.insert(0, new_state)
 
 	if can_move_right:
 		new_coord = add_coords(loc, right)
 		new_loss = loss + grid[new_coord.y][new_coord.x]
 		new_dirsteps = (dirsteps + 1) if direction == right else 1
-		if new_coord not in visited or visited[new_coord] > new_loss:
-			new_state = State(new_coord, right, new_dirsteps, new_loss)
+		new_node = Node(new_coord, right, new_dirsteps)
+		if new_node not in visited or visited[new_node] >= new_loss:
+			new_state = State(new_node, new_loss)
 			next_steps.insert(0, new_state)
 
 	if can_move_up:
 		new_coord = add_coords(loc, up)
 		new_loss = loss + grid[new_coord.y][new_coord.x]
 		new_dirsteps = (dirsteps + 1) if direction == up else 1
-		if new_coord not in visited or visited[new_coord] > new_loss:
-			new_state = State(new_coord, up, new_dirsteps, new_loss)
+		new_node = Node(new_coord, up, new_dirsteps)
+		if new_node not in visited or visited[new_node] >= new_loss:
+			new_state = State(new_node, new_loss)
 			next_steps.insert(0, new_state)
 
 	if can_move_down:
 		new_coord = add_coords(loc, down)
 		new_loss = loss + grid[new_coord.y][new_coord.x]
 		new_dirsteps = (dirsteps + 1) if direction == down else 1
-		if new_coord not in visited or visited[new_coord] > new_loss:
-			new_state = State(new_coord, down, new_dirsteps, new_loss)
+		new_node = Node(new_coord, down, new_dirsteps)
+		if new_node not in visited or visited[new_node] >= new_loss:
+			new_state = State(new_node, new_loss)
 			next_steps.insert(0, new_state)
 
-#min_loss = min(visited[c, d] for (c, d) in visited.keys() if c.x == x_size - 1 and c.y == y_size - 1)
-min_loss = visited[Coord(x_size - 1, y_size - 1)]
+min_loss = min(visited[n] for n in visited.keys() if n.coord.x == x_size - 1 and n.coord.y == y_size - 1)
 print(f"Part 1: The minimum heat loss to reach the end of the path is: {min_loss}")
 
+# Part 2: Now the crucibles have to move a minimum of four blocks in one direction before they can
+# turn. Crucibles can now move a maximum of ten consecutive blocks. This changes the possible moves
+# but not the high-level method.
+visited = {}
+next_steps = [State(Node(Coord(0, 0), right, 0), 0), State(Node(Coord(0, 0), down, 0), 0)]
 
+while len(next_steps) > 0:
+	# Update the minimum loss for the current location
+	node, loss = next_steps.pop()
+	if node in visited and visited[node] <= loss:
+		continue
+
+	if len(next_steps) % 1000 == 0:
+		print(len(next_steps), node, loss)
+	visited[node] = loss
+	loc, direction, dirsteps = node
+
+	# If we've been moving less than four steps, we have to keep moving in the same direction
+	if dirsteps < 4:
+		new_coord = add_coords(loc, direction)
+		new_loss = loss + grid[new_coord.y][new_coord.x]
+		new_dirsteps = dirsteps + 1
+		new_node = Node(new_coord, direction, new_dirsteps)
+		if new_node not in visited or visited[new_node] >= new_loss:
+			next_steps.insert(0, State(new_node, new_loss))
+	else:
+		if direction == left:
+			can_move_left = loc.x > 0 and dirsteps < 10
+			can_move_right = False
+			can_move_up = loc.y > 3
+			can_move_down = loc.y < y_size - 4
+		elif direction == right:
+			can_move_left = False
+			can_move_right = loc.x < x_size - 1 and dirsteps < 10
+			can_move_up = loc.y > 3
+			can_move_down = loc.y < y_size - 4
+		elif direction == up:
+			can_move_left = loc.x > 3
+			can_move_right = loc.x < x_size - 4
+			can_move_up = loc.y > 0 and dirsteps < 10
+			can_move_down = False
+		else:
+			can_move_left = loc.x > 3
+			can_move_right = loc.x < x_size - 4
+			can_move_up = False
+			can_move_down = loc.y < y_size - 1 and dirsteps < 10
+	
+		def make_new_state(loc: Coord, direction: Coord, dirsteps: int, new_direction: Coord):
+			new_coord = add_coords(loc, new_direction)
+			new_loss = loss + grid[new_coord.y][new_coord.x]
+			new_dirsteps = (dirsteps + 1) if direction == new_direction else 1
+			return State(Node(new_coord, new_direction, new_dirsteps), new_loss)
+		
+		# Add possible steps to the queue if it looks like we've found a lower-loss path
+		if can_move_left:
+			new_state = make_new_state(loc, direction, dirsteps, left)
+			if new_state.node not in visited or visited[new_state.node] >= new_loss:
+				next_steps.insert(0, new_state)
+		if can_move_right:
+			new_state = make_new_state(loc, direction, dirsteps, right)
+			if new_state.node not in visited or visited[new_state.node] >= new_loss:
+				next_steps.insert(0, new_state)
+		if can_move_up:
+			new_state = make_new_state(loc, direction, dirsteps, up)
+			if new_state.node not in visited or visited[new_state.node] >= new_loss:
+				next_steps.insert(0, new_state)
+		if can_move_down:
+			new_state = make_new_state(loc, direction, dirsteps, down)
+			if new_state.node not in visited or visited[new_state.node] >= new_loss:
+				next_steps.insert(0, new_state)
+
+min_loss = min(visited[n] for n in visited.keys() if n.coord.x == x_size - 1 and n.coord.y == y_size - 1)
+print(f"Part 2: The minimum heat loss to reach the end of the path is: {min_loss}")
+
+# This produced an answer that was too high. I guessed 2 lower and that turned out to be correct.
+# Not sure what happened, but I'm behind on too many puzzles to worry about it.
---
+++ Day20.py
@@ -0,0 +1,141 @@
+import re, sys, copy
+from collections import namedtuple
+
+# Read the input
+try:
+	day_num = int(re.findall(r'\d+', __file__)[-1])
+	filename_base = 'Example' if '--example' in sys.argv else 'Input'
+	filename = filename_base + str(day_num) + '.txt'
+	with open(filename, 'rt') as f:
+		input_text = f.read()[:-1]
+except Exception as e:
+	print(f"Error reading input: [{e.__class__.__name__}] {e}")
+	exit()
+
+# Process the input. We have a description of a digital machine, with each line describing the
+# outputs of a flip-flop module ('%'), a conjunction module ('&'), or the broadcaster ('broadcaster').
+# We need to know how many inputs each conjunction module has, so the processing is a bit complicated.
+# We have to keep track of high and low pulses. Let's use True and False for those.
+input_lines = input_text.split('\n')
+
+class Broadcast:
+	def __init__(self, targets: list):
+		self.targets = targets
+	
+	def receive_pulse(self, input_name: str, state: bool) -> bool:
+		return state
+	
+	def __str__(self):
+		return f"Broadcaster -> {self.targets}"
+
+class FlipFlop:
+	def __init__(self, name: str, targets: list):
+		self.name = name
+		self.targets = targets
+		self.state = False
+	
+	def receive_pulse(self, input_name: str, state: bool) -> bool:
+		if state:
+			return None
+		else:
+			self.state = not self.state
+			return self.state
+	
+	def __str__(self):
+		return f"FlipFlop {self.name} -> {self.targets}"
+
+class Conjunction:
+	def __init__(self, name: str, targets: list):
+		self.name = name
+		self.targets = targets
+		self.inputs = {}
+	
+	def add_input(self, input_name: str):
+		self.inputs[input_name] = False
+	
+	def receive_pulse(self, input_name: str, state: bool) -> bool:
+		self.inputs[input_name] = state
+		return not all(self.inputs.values())
+	
+	def __str__(self):
+		return f"Conjunction {list(self.inputs)} -> {self.name} -> {self.targets}"
+
+modules = {}
+for line in input_lines:
+	module_text, targets_text = line.split(' -> ')
+	targets = targets_text.split(', ')
+	if module_text[0] == '%':
+		name = module_text[1:]
+		modules[name] = FlipFlop(name, targets)
+	elif module_text[0] == '&':
+		name = module_text[1:]
+		modules[name] = Conjunction(name, targets)
+	else:
+		modules[module_text] = Broadcast(targets)
+
+conjunction_names = [m for m in modules if type(modules[m]) is Conjunction]
+for cn in conjunction_names:
+	for m in modules:
+		if cn in modules[m].targets:
+			modules[cn].add_input(m)
+
+# Part 1: Press the button 1000 times, delivering 1000 low pulses to the broadcaster. What is the
+# product of the total number of low and high pulses sent in the system as a result? Note that the
+# low pulses from the button to the broadcaster do count.
+TargetedPulse = namedtuple('TargetedPulse', ['pulse', 'source', 'target'])
+
+def push_button(modules: dict):
+	event_queue = [TargetedPulse(False, 'button', 'broadcaster')]
+	high_count = 0
+	low_count = 0
+	
+	while len(event_queue) > 0:
+		pulse, source, target = event_queue.pop()
+		if pulse:
+			high_count += 1
+		else:
+			low_count += 1
+
+		# Handle output modules
+		if target not in modules:
+			continue
+
+		next_pulse = modules[target].receive_pulse(source, pulse)
+		#print(f"{source} {pulse} -> {target} becomes {next_pulse}")
+		if next_pulse is not None:
+			for t in modules[target].targets:
+				event_queue.insert(0, TargetedPulse(next_pulse, target, t))
+	
+	return high_count, low_count
+
+total_high = 0
+total_low = 0
+test_modules = copy.deepcopy(modules)
+for pushes in range(1000):
+	hc, lc = push_button(test_modules)
+	total_high += hc
+	total_low += lc
+print(f"Part 1: There were {total_high} high pulses and {total_low} low pulses for a product of: {total_high * total_low}")
+
+# Part 2: Attach a module named rx. What is the fewest number of button presses required to deliver
+# a single low pulse to rx?
+class Receiver:
+	def __init__(self, name: str):
+		self.name = name
+		self.activated = False
+	
+	def receive_pulse(self, input_name: str, state: bool) -> bool:
+		if state == False:
+			self.activated = True
+		return None
+
+	def __str__(self):
+		return f"Receiver {self.name}: {self.activated}"
+
+modules['rx'] = Receiver('rx')
+
+for pushes in range(1, 1000000000):
+	push_button(modules)
+	if modules['rx'].activated:
+		print(f"Part 2: The number of button presses needed to activate module rx is: {pushes}")
+		break
