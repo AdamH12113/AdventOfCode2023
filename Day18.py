0f56db851d4c637af1869238d945e5e2efe9ee21
Adam Haun <adamhaun@gmail.com>
2023-12-27 23:29:42 -0600

    Completed day 18.

--- Day18.py
+++ Day18.py
@@ -1,4 +1,6 @@
 import re, sys, copy
+from dataclasses import dataclass
+from typing_extensions import Self
 
 # Read the input
 try:
@@ -74,7 +76,10 @@ print(f"Part 1: The size of the lagoon is: {len(dug_tiles)}")
 # Part 2: The color codes were the real instructions all along! The first five hex digits give the
 # distance and the last encodes the direction (0=R, 1=D, 2=L, 3=U). The lengths are now impossibly
 # large, and the example produces a lagoon with an area of ~950 billion. We'll probably have to find
-# bounding boxes or something. Ugh.
+# bounding boxes or something. Ugh. Doing some Googling (since this is apparently the Advent of
+# Googling for Algorithms this year) turns up something called the shoelace formula, which involves
+# adding determinants of 2x2 matrices constructed from pairs of coordinates moving clockwise around
+# the perimeter. Let's give it a try!
 new_dirs = []
 new_dists = []
 for color in colors:
@@ -88,5 +93,69 @@ for color in colors:
 	else:
 		new_dirs.append('U')
 
+# After doing day 24, I switched to dataclasses for coordinates. This code is originally from there.
+@dataclass
+class Vector:
+	x: int
+	y: int
+	
+	def __add__(self, v: Self) -> Self:
+		return type(self)(self.x + v.x, self.y + v.y)
 
+	def __sub__(self, v: Self) -> Self:
+		return type(self)(self.x - v.x, self.y - v.y)
+	
+	def __neg__(self) -> Self:
+		return type(self)(-self.x, -self.y)
+	
+	def __mul__(self, c: int) -> Self:
+		return type(self)(c * self.x, c * self.y)
+	
+	def __rmul__(self, c: int) -> Self:
+		return self * c
+	
+	def __str__(self) -> str:
+		return f"({self.x},{self.y})"
 
+up = Vector(0, 1)
+down = Vector(0, -1)
+left = Vector(-1, 0)
+right = Vector(1, 0)
+start = Vector(0, 0)
+
+# Let's convert the color-encoded directions to vectors
+directions = []
+perimeter = 0
+for color in colors:
+	new_dist = int(color[:5], 16)
+	if color[5] == '0':
+		new_dir = right
+	elif color[5] == '1':
+		new_dir = down
+	elif color[5] == '2':
+		new_dir = left
+	else:
+		new_dir = up
+	perimeter += new_dist
+	directions.append(new_dist * new_dir)
+
+# The directions seem to be in clockwise order. We want our coordinates to be in counter-clockwise
+# order for the algorithm.
+coords = [start]
+for d in reversed(directions):
+	c = coords[-1]
+	coords.append(c + d)
+
+# Time to tie some shoelaces!
+def det2x2(v1: Vector, v2: Vector):
+	return (v1.x * v2.y) - (v2.x * v1.y)
+
+area = 0
+for n in range(len(coords) - 1):
+	area += det2x2(coords[n], coords[n+1])
+area = area // 2
+
+# The area doesn't include the perimeter, which has a nonzero width. Curiously, it seems to be
+# missing *half* the perimeter. I'm not sure how that works, but I'm not going to complain. (There's
+# also an off-by-one, just for fun.)
+print(f"Part 2: The size of the much larger lagoon is: {area + perimeter//2 + 1}")
